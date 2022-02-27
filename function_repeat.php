013e4ea04f201b6c11748c42134afd21e45d301e
Slobodan <slobodan.novak@gmail.com>
2022-02-27 17:41:30 +0100

    added comment to function_repeat.php file

--- function_repeat.php
+++ function_repeat.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+
+//function is extended with additional argument for defining number of wanted repetitions 
 function repeat(array $array, int $repeat) : array {
     $array_with_repeated_elements = [];
     
